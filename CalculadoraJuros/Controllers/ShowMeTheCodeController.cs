3bb2c6a72c3dde443d68014ae526f1a1e3865a7a
Rodolfo Paes de Farias <rodolfo@movtech.inf.br>
2021-09-22 17:13:34 -0300

    Remoção arquivo

---
+++ CalculadoraJuros/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,35 @@
+﻿using CalculadoraJuros.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculadoraJuros.Controllers
+{
+    
+    [ApiController]
+    [Route("showmethecode")]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public ShowMeTheCodeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Retorna a URL do projeto no github
+        /// </summary>        
+        /// <returns></returns>
+        [HttpGet]
+        public string GetGitHubURL()
+        {
+            return _configuration["GitHubCodeURL"];
+        }
+    }
+}
